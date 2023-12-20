ab793ac8e36d81113ef6c23f8d5808a0c5af5dcc
Tymur Kosiak <us1gav@gmail.com>
2023-12-20 13:35:30 +0200

    Create homework lesson36.

---
+++ src/main/java/edu/hillel/springsecurityhwlesson36/SpringSecurityHwLesson36Application.java
@@ -0,0 +1,13 @@
+package edu.hillel.springsecurityhwlesson36;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHwLesson36Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityHwLesson36Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hillel/springsecurityhwlesson36/SpringSecurityHwLesson36ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hillel.springsecurityhwlesson36;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHwLesson36ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
